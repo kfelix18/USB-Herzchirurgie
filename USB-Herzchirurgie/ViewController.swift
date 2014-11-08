0271475e6ad811eee347dfd25ce1570c17a0f57c
Felix Felixa <kfelix18@gmail.com>
2014-11-08 10:34:32 +0100

    name label functionality

--- USB-Herzchirurgie/ViewController.swift
+++ USB-Herzchirurgie/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextfield: UITextField!
     @IBOutlet weak var enterMessageTextfield: UITextField!
     @IBOutlet weak var mailButton: UIButton!
-    
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     
@@ -36,7 +36,17 @@ class ViewController: UIViewController {
         enterMessageTextfield.text=""
         enterMessageTextfield.resignFirstResponder()
         
+
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextfield.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextfield.resignFirstResponder()
+        enterNameTextfield.text = ""
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
     }
 
 }
